8372ecab97be8fd1622f4fb487b9c0a4c0777d5d
Wu Jun <wujun4code@gmail.com>
2024-01-30 16:54:09 +0800

    feat: add middleware.

--- development/app/config/middlewares.ts
+++ development/app/config/middlewares.ts
@@ -9,4 +9,40 @@ export default [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "https://market-assets.strapi.io",
+            /**
+             * Note: If using a STORAGE_URL replace `https://${process.env.STORAGE_ACCOUNT}.blob.core.windows.net` w/ process.env.STORAGE_URL
+             * If using a CDN URL make sure to include that url in the CSP headers process.env.STORAGE_CDN_URL
+             */
+            `https://${process.env.STORAGE_ACCOUNT}.blob.core.windows.net`,
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            /**
+             * Note: If using a STORAGE_URL replace `https://${process.env.STORAGE_ACCOUNT}.blob.core.windows.net` w/ process.env.STORAGE_URL
+             * If using a CDN URL make sure to include that url in the CSP headers process.env.STORAGE_CDN_URL
+             */
+            `https://${process.env.STORAGE_ACCOUNT}.blob.core.windows.net`,
+
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
